1388709ebfa868be10f5b2d00ac6ff5f5655c455
dudusohne <dudu.hit@gmail.com>
2022-08-24 15:29:30 -0300

    add login route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
-    }
+    },
   ]
 })
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <Luisa :design="design" :config="config" v-model="viewModel" />
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  data: function () {
+    return {
+      design: {
+        figmaFile: "VzghS77t4KlMml9qgsoG5m",
+        figmaAccessKey: "figd_Pue4tzakacr6Xo2HD6MwDIPSmW9DJ1gIkaoK_UTw"
+      },
+      viewModel: {
+
+      },
+      config: {
+      }
+    }
+  }
+}
+</script>

6098fb5f7f45fa4963e23d259abdc6b01727108d
Eduardo Sohne <dudu.hit@gmail.com>
2022-08-25 11:39:29 -0300

    achiev two-way data-binding in login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,14 +8,28 @@ export default {
   data: function () {
     return {
       design: {
-        figmaFile: "VzghS77t4KlMml9qgsoG5m",
+        figmaFile: "b2o1iBDJakdeeD75Sg52wH",
         figmaAccessKey: "figd_Pue4tzakacr6Xo2HD6MwDIPSmW9DJ1gIkaoK_UTw"
       },
       viewModel: {
-
+        contact: {
+          email: '',
+          password: '',
+        },        
       },
       config: {
       }
+    }
+  },
+  methods: {
+    async buttonClick() {
+      const email = this.viewModel.contact.email
+      const password = this.viewModel.contact.password
+      console.log(email, password)
+      console.log('button working')
+    },
+    emailOnChange() {
+      console.log('email changing')
     }
   }
 }
